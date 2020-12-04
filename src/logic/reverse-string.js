a5a774665a9ae1d81de50ab755588a76a509efee
DIVYA SREE <67485922+DIVYASREE345@users.noreply.github.com>
2020-12-04 13:12:36 +0100

    final reverse-string

--- src/logic/reverse-string.js
+++ src/logic/reverse-string.js
@@ -7,11 +7,21 @@ console.log('--- loading logic --> ');
  * @param {string} [str=''] - the string to reverse
  * @returns {string} the reversed string
  */
-const reverseString = () => {};
+const reverseString = () => {
+  var str=favoriteWord;
+  var reversedString=''
+   for (var i = str.length - 1; i >= 0; i--) { 
+        reversedString+= str[i]; 
+    }
+    return reversedString
+    
+};
 
 {
   const consoleLog = console.log;
-  console.log = () => {};
+  console.log = () => {
+    
+  };
 
   try {
     console.assert(reverseString() === '', 'Test 0');
