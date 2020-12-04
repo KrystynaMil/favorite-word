48a87e8f7cd69202e46d8c75d25a4641ab3d3b14
KrystynaMil <kristina-manager@hotmail.com>
2020-12-04 12:12:56 +0100

    added clear-word.js

--- src/interactions/clear-word.js
+++ src/interactions/clear-word.js
@@ -6,4 +6,17 @@ console.log('--- loading interaction -->');
  * if there is a favorite word, the user is asked to confirm before clearing it.
  * if there is no saved word the user is told there is no word to clear
  */
-const clearWord = () => {};
+const clearWord = () => {
+
+    if (favoriteWord === ''){
+        alert('there is no word to clear');
+    }
+  
+    const confirmMessage = 'are you sure you want to clear "' + favoriteWord + '" ?';
+    const userConfirm = confirm(confirmMessage);
+    if (userConfirm){
+        favoriteWord = '';
+    } else {
+        favoriteWord = favoriteWord;
+    }
+};
